7dbbad808bcb5a5417dd4fdcbcfc9c2a81a570aa
rookielink <347252612@qq.com>
2026-06-25 01:02:08 +0800

    项目初始化,引入antd以及scss样式

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Button } from 'antd';
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Button>zhangfeng</Button>
+    </>
+  )
+}
+
+export default App
